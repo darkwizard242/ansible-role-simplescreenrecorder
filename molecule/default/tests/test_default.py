db285e8e9e9b5b65e803dfb38f588bee86e9b871
darkwizard242 <am900820@gmail.com>
2020-12-04 14:34:25 -0500

    testinfra functions refactored as part of migration

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,18 +6,33 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
+PACKAGE = 'simplescreenrecorder'
+PACKAGE_BINARY = '/usr/bin/simplescreenrecorder'
+
+
 def test_simplescreenrecorder_package_installed(host):
-    assert host.package("simplescreenrecorder").is_installed
+    """
+    Tests if simplescreenrecorder is installed.
+    """
+    assert host.package(PACKAGE).is_installed
 
 
-def test_simplescreenrecorder_binary_exists(host):
-    assert host.file('/usr/bin/simplescreenrecorder').exists
+def test_serverspec_binary_exists(host):
+    """
+    Tests if simplescreenrecorder binary exists.
+    """
+    assert host.file(PACKAGE_BINARY).exists
 
 
-def test_simplescreenrecorder_binary_file(host):
-    assert host.file('/usr/bin/simplescreenrecorder').is_file
+def test_serverspec_binary_file(host):
+    """
+    Tests if simplescreenrecorder binary is file type.
+    """
+    assert host.file(PACKAGE_BINARY).is_file
 
 
-def test_simplescreenrecorder_binary_which(host):
-    assert host.check_output('which simplescreenrecorder') == \
-      '/usr/bin/simplescreenrecorder'
+def test_serverspec_binary_which(host):
+    """
+    Tests the output to confirm simplescreenrecorder's binary location.
+    """
+    assert host.check_output('which simplescreenrecorder') == PACKAGE_BINARY
